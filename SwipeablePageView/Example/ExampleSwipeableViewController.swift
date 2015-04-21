e33859c55e1cb91d86f67ee654c34a8cb9699f0e
Chackle <chackle@gmail.com>
2015-04-21 21:10:41 +0100

    Example refined. Bug with rotating fixed. (Was adjusting header offset twice on rotate).

---
+++ SwipeablePageView/Example/ExampleSwipeableViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ExampleSwipeableViewController.swift
+//  SwipeablePageView
+//
+//  Created by Michael Green on 21/04/2015.
+//  Copyright (c) 2015 Michael Green. All rights reserved.
+//
+
+import UIKit
+
+class ExampleSwipeableViewController: MGSwipeableViewController {
+  
+  override func viewWillAppear(animated: Bool) {
+    super.viewWillAppear(animated)
+    var vc1 = ExampleViewController(nibName: "ExampleViewController", bundle: nil)
+    var vc2 = ExampleViewController(nibName: "ExampleViewController", bundle: nil)
+    var vc3 = ExampleViewController(nibName: "ExampleViewController", bundle: nil)
+    var vc4 = ExampleViewController(nibName: "ExampleViewController", bundle: nil)
+    var vc5 = ExampleViewController(nibName: "ExampleViewController", bundle: nil)
+    vc2.view.backgroundColor = UIColor.blueColor()
+    vc3.view.backgroundColor = UIColor.greenColor()
+    vc4.view.backgroundColor = UIColor.grayColor()
+    vc5.view.backgroundColor = UIColor.purpleColor()
+    self.addViewControllers([vc1, vc2, vc3, vc4, vc5], withTitles:["News Feed", "Some List", "Settings", "Profile", "Stuff"])
+  }
+}
--- SwipeablePageView/MGSwipeablePageView/MGSwipeableViewController.swift
+++ SwipeablePageView/MGSwipeablePageView/MGSwipeableViewController.swift
@@ -43,16 +43,6 @@ class MGSwipeableViewController: UIViewController, UIScrollViewDelegate, MGSwipe
     super.viewWillAppear(animated)
     self.view.setNeedsLayout()
     self.view.layoutIfNeeded()
-    var vc1 = ExampleViewController(nibName: "ExampleViewController", bundle: nil)
-    var vc2 = ExampleViewController(nibName: "ExampleViewController", bundle: nil)
-    var vc3 = ExampleViewController(nibName: "ExampleViewController", bundle: nil)
-    var vc4 = ExampleViewController(nibName: "ExampleViewController", bundle: nil)
-    var vc5 = ExampleViewController(nibName: "ExampleViewController", bundle: nil)
-    vc2.view.backgroundColor = UIColor.blueColor()
-    vc3.view.backgroundColor = UIColor.greenColor()
-    vc4.view.backgroundColor = UIColor.grayColor()
-    vc5.view.backgroundColor = UIColor.purpleColor()
-    self.addViewControllers([vc1, vc2, vc3, vc4, vc5], withTitles:["News Feed", "Some List", "Settings", "Profile", "Stuff"])
   }
 
   override func didReceiveMemoryWarning() {
@@ -66,7 +56,6 @@ class MGSwipeableViewController: UIViewController, UIScrollViewDelegate, MGSwipe
   override func willAnimateRotationToInterfaceOrientation(toInterfaceOrientation: UIInterfaceOrientation, duration: NSTimeInterval) {
     pageHeader.refreshViews()
     pageView.refreshViews()
-    pageHeader.snapToPagePosition(currentPagePosition)
     pageView.snapToPagePosition(currentPagePosition)
   }
   
